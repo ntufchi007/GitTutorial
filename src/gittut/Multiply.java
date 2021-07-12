7b1c8db22b02d719197165c7370f9c1cecc07be9
Nimish Tufchi <ntufchi007@gmail.com>
2021-07-12 14:42:45 +0530

    Adding multiply file

---
+++ src/gittut/Multiply.java
@@ -0,0 +1,12 @@
+package gittut;
+
+public class Multiply {
+	 public static void main(String [] args)
+	 {
+		 int a=2;
+		 int b=2;
+		 int mul =a*b;
+		 System.out.println("The prodect is "+mul);
+				 
+	 }
+}
